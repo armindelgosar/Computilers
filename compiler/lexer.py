f9c2f18b23d4ad7d5f9928d40578c786d1150959
armindelgosar <arminmaher@gmail.com>
2022-03-04 20:41:28 +0330

    feat(compiler): Add some tokens and regexes

--- compiler/lexer.py
+++ compiler/lexer.py
@@ -34,11 +34,6 @@ t_LPAREN = r'\('
 t_RPAREN = r'\)'
 t_ignore = ' \t'
 t_T_ID = r'[a-zA-Z_][a-zA-Z_0-9]*'
-t_T_DOUBLELITERAL = r'/^[0-9]+(\\.[0-9]+)?$'
-t_T_STRINGLITERAL = r'(?s).*'
-t_T_INTLITERAL = r'^\d+$'
-t_T_BOOLEANLITERAL = r'^(?i)(true|false)$'
-
 
 # Regular Expression rule with action
 # here is for action methods (for example setting value of lexeme for an integer)
