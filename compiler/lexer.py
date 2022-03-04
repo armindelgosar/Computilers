55f2cfae5edc5f73764b01f20c6245184706b107
armindelgosar <arminmaher@gmail.com>
2022-03-04 22:03:23 +0330

    feat(compiler): Add logical tokens and clear unneeded codes

--- compiler/lexer.py
+++ compiler/lexer.py
@@ -1,14 +1,6 @@
-# ------------------------------------------------------------
-# calclex.py
-#
-# tokenizer for a simple expression evaluator for
-# numbers and +,-,*,/
-# ------------------------------------------------------------
-
 import ply.lex as lex
 
-# list of token names
-# each token pattern which you want to add should be added to this tuple before all.
+
 tokens = (
     'T_INTLITERAL',
     'T_DOUBLELITERAL',
@@ -23,19 +15,49 @@ tokens = (
     'DIVIDE',
     'LPAREN',
     'RPAREN',
+    'LBRACE',
+    'RBRACE',
+    'LBRACKET',
+    'RBRACKET',
+    'SEMICOLON',
+    'BAR',
+    'EQUAL',
+    'LOGICAL_OR',
+    'LOGICAL_AND',
+    'LOGICAL_EQUAL',
+    'LOGICAL_NON_EQUAL',
+    'BIGGER_THAN',
+    'BIGGER_THAN_OR_EQUAL',
+    'SMALLER_THAN',
+    'SMALLER_THAN_OR_EQUAL',
+    'COMMA',
     'ignore',
     'newline',
     'error',
 )
 
-# Regular Expressions
-# here is the regex patterns which should we define for every token and should start with 't_'
 t_PLUS = r'\+'
-t_MINUS = r'-'
+t_MINUS = r'\-'
 t_TIMES = r'\*'
 t_DIVIDE = r'/'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
+t_LBRACE = r'\{'
+t_RBRACE = r'\}'
+t_LBRACKET = r'\['
+t_RBRACKET = r'\]'
+t_SEMICOLON = r'\;'
+t_BAR = r'\|'
+t_EQUAL = r'\='
+t_LOGICAL_OR = r'\|\|'
+t_LOGICAL_AND = r'\&\&'
+t_LOGICAL_EQUAL = r'\=\='
+t_LOGICAL_NON_EQUAL = r'\!\='
+t_BIGGER_THAN = r'\>'
+t_BIGGER_THAN_OR_EQUAL = r'\>\='
+t_SMALLER_THAN = r'\<'
+t_SMALLER_THAN_OR_EQUAL = r'\<\='
+t_COMMA = r'\,'
 t_ignore = ' \t'
 t_T_ID = r'[a-zA-Z_][a-zA-Z_0-9]*'
 t_T_DOUBLELITERAL = r'/^[0-9]+(\\.[0-9]+)?$'
@@ -46,11 +68,6 @@ t_DEFINE = r'define'
 t_IMPORT = r'import'
 
 
-# Regular Expression rule with action
-# here is for action methods (for example setting value of lexeme for an integer)
-
-
-# Define a rule so we can track line numbers
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
@@ -62,14 +79,9 @@ def t_T_INTLITERAL(t):
     return t
 
 
-# A string containing ignored characters (spaces and tabs)
-
-
-# Error handling rule
 def t_error(t):
     print("UNDEFINED_TOKEN")
     t.lexer.skip(1)
 
 
-# Build the lexer
 lexer = lex.lex()
