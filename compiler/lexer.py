b7cc9e9f903a419c6dda8be6273e7ca0cb503e2e
armindelgosar <arminmaher@gmail.com>
2022-03-04 20:49:25 +0330

    feat(compiler): Add some tokens and regexes

--- compiler/lexer.py
+++ compiler/lexer.py
@@ -15,13 +15,15 @@ tokens = (
     'T_STRINGLITERAL',
     'T_BOOLEANLITERAL',
     'T_ID',
-    'PLUS ',
+    'PLUS',
     'MINUS',
     'TIMES',
-    'DIVID',
-    'LPARE',
-    'RPARE',
+    'DIVIDE',
+    'LPAREN',
+    'RPAREN',
     'ignore',
+    'newline',
+    'error',
 )
 
 # Regular Expressions
@@ -34,32 +36,19 @@ t_LPAREN = r'\('
 t_RPAREN = r'\)'
 t_ignore = ' \t'
 t_T_ID = r'[a-zA-Z_][a-zA-Z_0-9]*'
+t_T_DOUBLELITERAL = r'/^[0-9]+(\\.[0-9]+)?$'
+t_T_STRINGLITERAL = r'(?s).+'
+t_T_INTLITERAL = r'^\d+$'
+t_T_BOOLEANLITERAL = r'^(?i)(true|false)$'
+
 
 # Regular Expression rule with action
 # here is for action methods (for example setting value of lexeme for an integer)
 
-def t_T_INTLITERAL(t):
-    t.value = int(t.value)
-    return t
-
-
-def t_T_DOUBLELITERAL(t):
-    t.value = float(t.value)
-    return t
-
-
-def t_T_STRINGLITERAL(t):
-    t.value = str(t.value)
-    return t
-
-
-def t_T_BOOLEANLITERAL(t):
-    t.value = bool(t.value)
-    return t
-
 
 # Define a rule so we can track line numbers
 def t_newline(t):
+    r'\n+'
     t.lexer.lineno += len(t.value)
 
 
