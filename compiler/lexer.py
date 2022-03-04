8f759324c01207738fc2f4e1fc11e2ae32090bd7
seyyedAlirezaGhazanfari <59168138+seyyedAlirezaGhazanfari@users.noreply.github.com>
2022-03-04 20:10:24 +0330

    docs(scanner): Add structure comments

--- compiler/lexer.py
+++ compiler/lexer.py
@@ -8,16 +8,19 @@
 import ply.lex as lex
 
 # list of token names
+# each token pattern which you want to add should be added to this tuple before all.
 tokens = (
     'PLUS',
     'MINUS',
 )
 
 # Regular Expressions
+# here is the regex patterns which should we define for every token and should start with 't_'
 t_PLUS = r'\+'
 t_MINUS = r'-'
 
 # Regular Expression rule with action
+# here is for action methods (for example setting value of lexeme for an integer)
 
 # Build the lexer
 lexer = lex.lex()
--- compiler/main.py
+++ compiler/main.py
@@ -3,6 +3,7 @@ from os import environ as env
 from dotenv import load_dotenv
 
 
+# run scanner method
 def run(input_file_address: str) -> None:
     with open(input_file_address) as input_file:
         data = input_file.read()
@@ -16,5 +17,11 @@ def run(input_file_address: str) -> None:
 
 
 if __name__ == '__main__':
+    # we have a .env file that put our fixed strings on it.
     load_dotenv('../.env')
+    # runnable part of project
+
+    # run preprocessor
+
+    # run scanner
     run(input_file_address=env['SCANNER_INPUT_FILE_ADDRESS'])
