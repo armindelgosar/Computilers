2f6592c5bcfa51fc5cd663d3bc2a4ae13c4147e6
armindelgosar <arminmaher@gmail.com>
2022-03-12 17:14:41 +0330

    fix(compiler): Fix string regex

--- compiler/preprocessor.py
+++ compiler/preprocessor.py
@@ -6,12 +6,13 @@ def preprocess(input_data: str, replacement_tokens):
     for replacement_token in replacement_tokens:
         identifier = replacement_token[0]
         replaced = replacement_token[1]
-        input_data = input_data.replace(identifier, replaced)
+        input_data = re.sub(r'([^a-zA-Z0-9\"])({})([^a-zA-Z0-9\"])'.format(identifier), r'\g<1>{}\g<3>'.format(replaced), input_data)
+        input_data = re.sub(r'([^a-zA-Z0-9\"])({})([^a-zA-Z0-9\"])'.format(identifier), r'\g<1>{}\g<3>'.format(replaced), input_data)
     return input_data
 
 
 def factorize_preprocess(input_data: str):
-    define_regex = r'define (\S*)\s(.*)'
+    define_regex = r'define (\S*)\s(.*)\W'
     matched_patterns = re.findall(define_regex, input_data)
     return matched_patterns
 
