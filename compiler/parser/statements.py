9ab2bcdf17dd1428467f3df4a2b5c787d0bbbc91
seyyedAlirezaGhazanfari <59168138+seyyedAlirezaGhazanfari@users.noreply.github.com>
2022-05-09 11:58:22 +0430

    feat(parser): Add while and if stmt

--- compiler/parser/statements.py
+++ compiler/parser/statements.py
@@ -6,3 +6,15 @@ def p_continue_stmt(p):
 def p_break_stmt(p):
     'BreakStmt : BREAK SEMICOLON'
     # add semantic
+
+
+def p_if_stmt(p):
+    '''IfStmt : IF LPAREN Expr RPAREN Stmt
+    | IF LPAREN Expr RPAREN Stmt ELSE Stmt
+    '''
+    # Semantic definition
+
+
+def p_while_stmt(p):
+    'WhileStmt : WHILE LPAREN Expr RPAREN Stmt'
+    # Semantic definition
