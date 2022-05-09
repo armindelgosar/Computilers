feebf424d786e15c368b42df066a85c7f5cc4b74
seyyedAlirezaGhazanfari <59168138+seyyedAlirezaGhazanfari@users.noreply.github.com>
2022-05-09 12:17:00 +0430

    feat(parser): Add for stmt

--- compiler/parser/statements.py
+++ compiler/parser/statements.py
@@ -1,13 +1,3 @@
-def p_continue_stmt(p):
-    'ContinueStmt : CONTINUE SEMICOLON'
-    # p[0] = ContinueStmt(p[1])
-
-
-def p_break_stmt(p):
-    'BreakStmt : BREAK SEMICOLON'
-    # add semantic
-
-
 def p_if_stmt(p):
     '''IfStmt : IF LPAREN Expr RPAREN Stmt
     | IF LPAREN Expr RPAREN Stmt ELSE Stmt
@@ -18,3 +8,27 @@ def p_if_stmt(p):
 def p_while_stmt(p):
     'WhileStmt : WHILE LPAREN Expr RPAREN Stmt'
     # Semantic definition
+
+
+def p_for_stmt(p):
+    '''ForStmt : FOR LPAREN Expr SEMICOLON Expr SEMICOLON Expr RPAREN Stmt
+    | FOR LPAREN SEMICOLON Expr SEMICOLON Expr RPAREN Stmt
+    | FOR LPAREN Expr SEMICOLON Expr SEMICOLON RPAREN Stmt
+    FOR LPAREN SEMICOLON Expr SEMICOLON RPAREN Stmt
+    '''
+    # semantic definition
+
+
+def p_return_stmt(p):
+    'ReturnStmt: RETURN SEMICOLON | RETURN Expr SEMICOLON'
+    # semantic definition
+
+
+def p_break_stmt(p):
+    'BreakStmt : BREAK SEMICOLON'
+    # add semantic
+
+
+def p_continue_stmt(p):
+    'ContinueStmt : CONTINUE SEMICOLON'
+    # p[0] = ContinueStmt(p[1])
